5ab55e918a59351248c04944293e84a004a8cf2e
Vijay Subramaniyam <vijayvs6383@gmail.com>
2024-02-15 22:47:02 +0530

    Task-2

---
+++ netflix-clone/script.js
@@ -0,0 +1,7 @@
+let faqelement = document.getElementById('faq-question');
+
+faqelement.addEventListener('click',() =>{
+    let faqdescriptionelement = document.getElementById('faq-description');
+    console.log(faqdescriptionelement.style.display)
+   
+})
