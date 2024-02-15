fc7e1a32c4295e850f5a48c12bffcb7100411850
Vijay Subramaniyam <vijayvs6383@gmail.com>
2024-02-15 22:50:46 +0530

    Delete script.js

--- netflix-clone/script.js
+++
@@ -1,7 +0,0 @@
-let faqelement = document.getElementById('faq-question');
-
-faqelement.addEventListener('click',() =>{
-    let faqdescriptionelement = document.getElementById('faq-description');
-    console.log(faqdescriptionelement.style.display)
-   
-})
